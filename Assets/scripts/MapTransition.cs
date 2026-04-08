0e7960996be52b7e774cb24097241bcd34ea84e1
baczek2008 <minecraftmobile123456@gmail.com>
2026-04-08 18:46:34 +0200

    bug fix
     dziala przechodzenie miedzy pokojami nie przesuwa sie nic

--- Assets/scripts/MapTransition.cs
+++ Assets/scripts/MapTransition.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using Cinemachine;
 
-public class MapTransition : MonoBehaviour
+public class CameraDirectionalTransition : MonoBehaviour
 {
-    [SerializeField] PolygonCollider2D mapBoundry;
-    CinemachineConfiner confiner;
-    [SerializeField] Direction direction;
-    enum Direction { Up,Down,Left,Right }
+    public enum Axis { Horizontal, Vertical }
+
+    [SerializeField] private Axis axis = Axis.Horizontal;
+    [SerializeField] private PolygonCollider2D roomBegin;
+    [SerializeField] private PolygonCollider2D roomEnd;
+
+    private CinemachineConfiner confiner;
+    private Vector3 lastPlayerPos;
+    private bool canSwitch = true;
+
     private void Awake()
     {
         confiner = FindObjectOfType<CinemachineConfiner>();
@@ -14,29 +20,51 @@ public class MapTransition : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (!collision.CompareTag("Player") || !canSwitch)
+            return;
+
+        Vector3 currentPos = collision.transform.position;
+
+        PolygonCollider2D targetRoom;
+
+        if (axis == Axis.Horizontal)
         {
-            confiner.m_BoundingShape2D = mapBoundry;
-            UpdatePlayerPosition(collision.gameObject);
+            // sprawdzamy kierunek ruchu X
+            if (currentPos.x > lastPlayerPos.x)
+                targetRoom = roomEnd;   // idzie w prawo
+            else
+                targetRoom = roomBegin; // idzie w lewo
+        }
+        else
+        {
+            // sprawdzamy kierunek ruchu Y
+            if (currentPos.y > lastPlayerPos.y)
+                targetRoom = roomEnd;   // idzie w górê
+            else
+                targetRoom = roomBegin; // idzie w dó³
+        }
+
+        confiner.m_BoundingShape2D = targetRoom;
+        confiner.InvalidatePathCache();
+
+        canSwitch = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canSwitch = true;
         }
     }
-    private void UpdatePlayerPosition(GameObject player)
+
+    private void Update()
     {
-        Vector3 newPos = player.transform.position;
-        switch (direction) {
-            case Direction.Up:
-                newPos.y += 2;
-                break;
-            case Direction.Down:
-                newPos.y -= 2;
-                break;
-            case Direction.Left:
-                newPos.x += 2;
-                break;
-            case Direction.Right:
-                newPos.x -= 2;
-                break;
+        // zapisujemy ostatni¹ pozycjê gracza
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            lastPlayerPos = player.transform.position;
         }
-        player.transform.position = newPos;
     }
 }
